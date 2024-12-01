bc5d0396fed0e95c5baf8c6bb2a94868bb310bd4
dinyudin203 <[sungyujin2003@gmail.com]>
2024-12-01 17:49:25 +0900

    add header css

--- src/components/OkrPageHeader.js
+++ src/components/OkrPageHeader.js
@@ -1,24 +1,28 @@
 import React from 'react';
-
+import '../styles/OkrPageHeader.css';
 const OkrPageHeader = ({ activeTab, setActiveTab }) => {
   return (
     <header>
-      <button
-        onClick={() => setActiveTab('OkrInfoPage')}
-      >
-        OKR 기업정보
-      </button>
-      <button
-        onClick={() => setActiveTab('OkrDataPage')}
-      >
-        OKR 데이터 목록
-      </button>
-      <button
-        onClick={() => setActiveTab('OkrAIPage')}
-      >
-        AI 적용
-      </button>
+        <h1>Equalsum</h1>
+        <div className="nav-buttons">
+          <button
+            onClick={() => setActiveTab('OkrInfoPage')}
+          >
+            OKR 기업정보
+          </button>
+          <button
+            onClick={() => setActiveTab('OkrDataPage')}
+          >
+            OKR 데이터 목록
+          </button>
+          <button
+            onClick={() => setActiveTab('OkrAIPage')}
+          >
+            AI 적용
+          </button>
+        </div>
     </header>
+    
   );
 };
 
